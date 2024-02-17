52806e1ee1cb5e0abbc3fe0de065da06a59db482
anarodriguezm <146813513+anarodriguezm@users.noreply.github.com>
2024-02-17 11:29:38 +0100

    Add files via upload

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -1,1 +1,70 @@
+#!/bin/bash
+# Creacion y configuracion de la base de datos para wordpress
+
+mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
+mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
+mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY 
+'$WORDPRESS_DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO 
+$WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
+
+# Paso 3: Configuración de Apache2
+# Añadir directivas al archivo de configuración de Apache (000-default.conf)
+sed -i '/DocumentRoot \/var\/www\/html/a\
+\
+  <Directory /var/www/html/>\
+    AllowOverride All\
+  </Directory>' /etc/apache2/sites-available/000-default.conf
+
+# Activar el mod_rewrite para enlaces permanentes de WordPress
+a2enmod rewrite
+
+# Reiniciar el servidor web Apache
+systemctl restart apache2
+
+# Paso 4: Descarga e instalación de WordPress
+# Instalación de wget para descargar los archivos de WordPress
+apt install -y wget
+
+# Descargar WordPress
+wget https://es.wordpress.org/latest-es_ES.zip -P /tmp
+
+# Instalación de unzip para descomprimir el archivo de WordPress
+apt install -y unzip
+
+# Descomprimir WordPress y mover los archivos al directorio de Apache
+unzip /tmp/latest-es_ES.zip -d /tmp
+mv -f /tmp/wordpress/* /var/www/html
+
+# Cambiar el propietario de los archivos de WordPress
+chown -R www-data:www-data /var/www/html
+
+# Reiniciar el servidor web Apache
+systemctl restart apache2
+
+# Preparar la configuración para los enlaces permanentes de WordPress
+echo "
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+" > /var/www/html/.htaccess
+
+# Crear el archivo de configuración wp-config.php
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# Configurar las variables de configuración en el archivo wp-config.php
+sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wp-config.php
+sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wp-config.php
+sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wp-config.php
+
 
--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -1,1 +1,48 @@
+#!/bin/bash
+
+# Paso 1: Actualización de repositorios
+apt update
+
+# Paso 2: Instalación de Apache2
+apt install apache2 -y
+
+# Paso 3: Instalación de PHP y módulos
+apt install php libapache2-mod-php php-mysql -y
+
+# Paso 4: Edición del archivo de configuración de Apache (000-default.conf)
+echo '
+<VirtualHost *:80>
+#ServerName www.example.com
+ServerAdmin webmaster@localhost
+DocumentRoot /var/www/html
+DirectoryIndex index.html index.php
+ErrorLog ${APACHE_LOG_DIR}/error.log
+CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+' > /etc/apache2/sites-available/000-default.conf
+
+# Paso 5: Reinicio del servicio Apache2
+systemctl restart apache2
+
+# Paso 6: Creación de una página de prueba PHP
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
+# Paso 7: Instalación de MariaDB
+# Configurar la contraseña de root de MariaDB
+debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password password $MARIADB_ROOT_PASSWORD"
+debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password_again password $MARIADB_ROOT_PASSWORD"
+apt install -y mariadb-server mariadb-client
+
+# Paso 8: Instalación de PhpMyAdmin
+# Configurar la contraseña de PhpMyAdmin
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+# Establecemos las contraseñas para mariadb y phpmyadmin
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PHPMYADMIN_PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $MARIADB_ROOT_PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_PASSWORD"
+# Con esto seleccionamos la opcion que salia en grafico de apache2
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+# Realizamos la instalacion
+apt install -y phpmyadmin php-mbstring php-zip php-gd php-json php-curl
+
 
